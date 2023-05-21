d1459112fa8e4d415411abcaf93e0ce7f5a3f7c8
unknown <daamerr@proton.me>
2023-05-21 03:36:11 +0300

    Lint accountController.

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,174 +1,185 @@
-const Account = require('../models/account');
-const Transaction = require('../models/transaction.js');
-const sequelize = require('../db.js');
+const Account = require("../models/account");
+const Transaction = require("../models/transaction.js");
+const sequelize = require("../db.js");
 
 exports.createAccount = async (req, res) => {
-    try {
-      const { initialBalance } = req.body;
-      const parsedBalance = parseFloat(initialBalance); // Parse the initial balance as a number
-  
-      if (isNaN(parsedBalance)) {
-        return res.status(400).json({ error: 'Invalid initial balance.' });
-      }
-  
-      const account = await Account.create({ balance: parsedBalance });
-  
-      res.status(201).json({ accountId: account.id, balance: account.balance });
-    } catch (error) {
-      console.error('Error creating an account:', error);
-      res.status(500).json({ error: 'Failed to create an account.' });
+  try {
+    const { initialBalance } = req.body;
+    const parsedBalance = parseFloat(initialBalance);
+
+    if (isNaN(parsedBalance)) {
+      return res.status(400).json({ error: "Invalid initial balance." });
     }
-  };
-  
+
+    const account = await Account.create({ balance: parsedBalance });
+
+    res.status(201).json({ accountId: account.id, balance: account.balance });
+  } catch (error) {
+    console.error("Error creating an account:", error);
+    res.status(500).json({ error: "Failed to create an account." });
+  }
+};
+
 exports.depositToAccount = async (req, res) => {
+  try {
+    const { accountId, amount } = req.body;
+
+    const transaction = await sequelize.transaction();
+
     try {
-      const { accountId, amount } = req.body;
-  
-      const transaction = await sequelize.transaction();
-  
-      try {
-        const account = await Account.findByPk(accountId, { transaction });
-  
-        if (!account) {
-          await transaction.rollback();
-          return res.status(404).json({ error: 'Account not found.' });
-        }
+      const account = await Account.findByPk(accountId, { transaction });
 
-        
-        account.balance = Number(account.balance) + Number(amount);
+      if (!account) {
+        await transaction.rollback();
+        return res.status(404).json({ error: "Account not found." });
+      }
 
-        await account.save({ transaction });
-  
-        await Transaction.create({
+      account.balance = Number(account.balance) + Number(amount);
+
+      await account.save({ transaction });
+
+      await Transaction.create(
+        {
           source_account_id: null,
           destination_account_id: accountId,
-          type: 'deposit',
+          type: "deposit",
           amount,
-        }, { transaction });
-  
-        await transaction.commit();
-  
-        res.json({ balance: account.balance });
-      } catch (error) {
-        await transaction.rollback();
-        throw error;
-      }
+        },
+        { transaction }
+      );
+
+      await transaction.commit();
+
+      res.json({ balance: account.balance });
     } catch (error) {
-      res.status(500).json({ error: 'Failed to deposit.' });
+      await transaction.rollback();
+      throw error;
     }
-  };
+  } catch (error) {
+    res.status(500).json({ error: "Failed to deposit." });
+  }
+};
 
 exports.withdrawMoney = async (req, res) => {
-    try {
-      const { accountId, amount } = req.body;
-  
-      const transaction = await sequelize.transaction();
-  
-      try {
-        const account = await Account.findByPk(accountId, { transaction });
-  
-        if (!account) {
-          await transaction.rollback();
-          return res.status(404).json({ error: 'Account not found.' });
-        }
+  try {
+    const { accountId, amount } = req.body;
 
-        if (account.balance < amount) {
-            await transaction.rollback();
-            return res.status(400).json({ error: 'Insufficient funds.' });
-        }
-  
-        account.balance -= amount;
-        await account.save({ transaction });
-  
-        await Transaction.create({
+    const transaction = await sequelize.transaction();
+
+    try {
+      const account = await Account.findByPk(accountId, { transaction });
+
+      if (!account) {
+        await transaction.rollback();
+        return res.status(404).json({ error: "Account not found." });
+      }
+
+      if (account.balance < amount) {
+        await transaction.rollback();
+        return res.status(400).json({ error: "Insufficient funds." });
+      }
+
+      account.balance -= amount;
+      await account.save({ transaction });
+
+      await Transaction.create(
+        {
           source_account_id: null,
           destination_account_id: accountId,
-          type: 'deposit',
+          type: "deposit",
           amount,
-        }, { transaction });
-  
-        await transaction.commit();
-  
-        res.json({ balance: account.balance });
-      } catch (error) {
-        await transaction.rollback();
-        throw error;
-      }
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to deposit.' });
-    }
-  };
+        },
+        { transaction }
+      );
 
-  exports.transferMoney = async (req, res) => {
-    let transaction = null;
-  
+      await transaction.commit();
+
+      res.json({ balance: account.balance });
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to deposit." });
+  }
+};
+
+exports.transferMoney = async (req, res) => {
+  let transaction = null;
+
+  try {
+    const { sourceAccountId, destinationAccountId, amount } = req.body;
+
     try {
-      const { sourceAccountId, destinationAccountId, amount } = req.body;
-  
-      try {
-        const transaction = await sequelize.transaction();
-  
-        const sourceAccount = await Account.findByPk(sourceAccountId, { transaction });
-        const destinationAccount = await Account.findByPk(destinationAccountId, { transaction });
-  
-        if (!sourceAccount || !destinationAccount) {
-          await transaction.rollback();
-          return res.status(404).json({ error: 'Account not found.' });
-        }
-  
-        if (sourceAccount.balance < amount) {
-          await transaction.rollback();
-          return res.status(400).json({ error: 'Insufficient funds.' });
-        }
-  
-        sourceAccount.balance -= amount;
-        destinationAccount.balance = Number(destinationAccount.balance) + Number(amount);
-  
-        await sourceAccount.save({ transaction });
-        await destinationAccount.save({ transaction });
-  
-        const newTransaction = await Transaction.create(
-          {
-            source_account_id: sourceAccountId,
-            destination_account_id: destinationAccountId,
-            type: 'transfer',
-            amount,
-          },
-          { transaction }
-        );
-  
-        await transaction.commit();
-  
-        res.json({
-          sourceAccountBalance: sourceAccount.balance,
-          destinationAccountBalance: destinationAccount.balance,
-        });
-      } catch (error) {
-        if (transaction) {
-          await transaction.rollback();
-        }
-        throw error;
+      const transaction = await sequelize.transaction();
+
+      const sourceAccount = await Account.findByPk(sourceAccountId, {
+        transaction,
+      });
+      const destinationAccount = await Account.findByPk(destinationAccountId, {
+        transaction,
+      });
+
+      if (!sourceAccount || !destinationAccount) {
+        await transaction.rollback();
+        return res.status(404).json({ error: "Account not found." });
       }
+
+      if (sourceAccount.balance < amount) {
+        await transaction.rollback();
+        return res.status(400).json({ error: "Insufficient funds." });
+      }
+
+      sourceAccount.balance -= amount;
+      destinationAccount.balance =
+        Number(destinationAccount.balance) + Number(amount);
+
+      await sourceAccount.save({ transaction });
+      await destinationAccount.save({ transaction });
+
+      const newTransaction = await Transaction.create(
+        {
+          source_account_id: sourceAccountId,
+          destination_account_id: destinationAccountId,
+          type: "transfer",
+          amount,
+        },
+        { transaction }
+      );
+
+      await transaction.commit();
+
+      res.json({
+        sourceAccountBalance: sourceAccount.balance,
+        destinationAccountBalance: destinationAccount.balance,
+      });
     } catch (error) {
       if (transaction) {
         await transaction.rollback();
       }
       throw error;
     }
-  };
-  
-
-  exports.getAccountBalance = async (req, res) => {
-    try {
-      const { accountId } =  req.params;
-  
-      const account = await Account.findByPk(accountId);
-      if (!account) {
-        return res.status(404).json({ error: 'Account not found.' });
-      }
-      res.json({ balance: account.balance });
-    } catch (error) {
-      console.error('Error getting account balance:', error);
-      res.status(500).json({ error: 'Failed to get account balance.' });
+  } catch (error) {
+    if (transaction) {
+      await transaction.rollback();
     }
-  };
+    throw error;
+  }
+};
+
+exports.getAccountBalance = async (req, res) => {
+  try {
+    const { accountId } = req.params;
+
+    const account = await Account.findByPk(accountId);
+
+    if (account === null) {
+      return res.status(404).json({ error: "Account not found." });
+    }
+
+    res.json({ balance: account.balance });
+  } catch (error) {
+    console.error("Error getting account balance:", error);
+    res.status(500).json({ error: "Failed to get account balance." });
+  }
+};
