8dc377fef2ad2b50a1c77521731491383f9dbe2e
d-a-b-d <daamerr@proton.me>
2025-06-22 17:11:54 +0300

    add Deposit Money test

--- test/integration/accountRoutes.test.js
+++ test/integration/accountRoutes.test.js
@@ -7,6 +7,8 @@ beforeAll(async () => {
   await app.ready();
 });
 
+
+
 describe("Create Account", () => {
   test("should create a new account", async () => {
     const result = await request(app.server) 
@@ -16,6 +18,18 @@ describe("Create Account", () => {
     expect(result.status).toBe(201);
     expect(result.body).toHaveProperty("accountId");
     expect(result.body).toHaveProperty("balance");
+  });
+});
+
+describe("Deposit Money", () => {
+  test("should deposit the amount of money successfully", async () => {
+    const result = await request(app.server)
+      .post("/accounts/deposit")
+      .send({ amount: 1000, accountId: 1 });
+
+
+    expect(result.status).toBe(200);
+    expect(result.body).toHaveProperty("newBalance");
   });
 });
 
